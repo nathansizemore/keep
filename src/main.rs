6d7415a251fc30f7274b0c3287303ea1421778b0
Nathan Sizemore <nathanrsizemore@gmail.com>
2017-03-14 21:03:54 -0400

    Formatting.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 // Copyright 2016 Nathan Sizemore <nathanrsizemore@gmail.com>
 //
-// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
-// If a copy of the MPL was not distributed with this file,
-// You can obtain one at http://mozilla.org/MPL/2.0/.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// you can obtain one at http://mozilla.org/MPL/2.0/.
 
 
 extern crate docopt;
